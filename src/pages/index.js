772bacbb915f17a52c6f4d9abc75155c29a149f7
Tom D <dunerz@hotmail.com>
2021-04-09 17:29:29 +1000

    playing around with Gatsby and refamiliarising with react

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Header from '../components/header'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  return (
+    <Layout>     
+      <Header headerText='Hello, World!' />
+      <Link to="/contact/">Contact</Link><br />
+      <Link to="/about/">About</Link>
+    </Layout>
+  )
 }
