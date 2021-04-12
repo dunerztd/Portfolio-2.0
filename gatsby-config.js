0c004cccb51f067f436de132b79060e74e4b641d
Tom D <dunerz@hotmail.com>
2021-04-12 15:01:25 +1000

    small change

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,13 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-// module.exports = {
-//   plugins: [
-//     {
-//       resolve: `gatsby-plugin-typography`,
-//       options: {
-//         pathToConfigModule: `src/utils/typography`,
-//       },
-//     },
-//   ],
-// }
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
