fbb74cab3baad9f69588750e4bf1752032b0ce67
Tom D <dunerz@hotmail.com>
2021-04-14 16:30:41 +1000

    Completed the 'Work' page. Conversion of portfolio site to react is complete

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,45 +12,45 @@ export default function Blog() {
     <Layout>
       <Header headerText="Blog" />
       <body>
-        <div class={styles.container}>
+        <div className={styles.container}>
 
-          <section class={styles.mainView}>
+          <section className={styles.mainView}>
 
-            <div class={styles.linksContainer}>
+            <div className={styles.linksContainer}>
               <h2>Recent Posts</h2>
-                <ul class={styles.recentPosts}>
-                  <li class={styles.recentEntry}>
-                    <a href="#entry-1" class={styles.recentEntryName}>Conjugate the verb!</a>
-                    <div class={styles.recentEntryDate}>31/03/2020</div>
+                <ul className={styles.recentPosts}>
+                  <li className={styles.recentEntry}>
+                    <a href="#entry-1" className={styles.recentEntryName}>Conjugate the verb!</a>
+                    <div className={styles.recentEntryDate}>31/03/2020</div>
                   </li>
-                  <li class={styles.recentEntry}>
-                    <a href="#entry-2" class={styles.recentEntryName}>Everyone loves latin!</a>
-                    <div class={styles.recentEntryDate}>31/03/2020</div>
+                  <li className={styles.recentEntry}>
+                    <a href="#entry-2" className={styles.recentEntryName}>Everyone loves latin!</a>
+                    <div className={styles.recentEntryDate}>31/03/2020</div>
                   </li>
-                  <li class={styles.recentEntry}>
-                    <a href="#entry-3" class={styles.recentEntryName}>What have the Romans ever done for us?</a>
-                    <div class={styles.recentEntryDate}>31/03/2020</div>
+                  <li className={styles.recentEntry}>
+                    <a href="#entry-3" className={styles.recentEntryName}>What have the Romans ever done for us?</a>
+                    <div className={styles.recentEntryDate}>31/03/2020</div>
                   </li>
-                  <li class={styles.recentEntry}>
-                    <a href="#entry-4" class={styles.recentEntryName}>Learn a dead language??</a>
-                    <div class={styles.recentEntryDate}>31/03/2020</div>
+                  <li className={styles.recentEntry}>
+                    <a href="#entry-4" className={styles.recentEntryName}>Learn a dead language??</a>
+                    <div className={styles.recentEntryDate}>31/03/2020</div>
                   </li>
-                  <li class={styles.recentEntry}>
-                    <a href="#entry-5" class={styles.recentEntryName}>I'm moving to the Vatican City!</a>
-                    <div class={styles.recentEntryDate}>31/03/2020</div>
+                  <li className={styles.recentEntry}>
+                    <a href="#entry-5" className={styles.recentEntryName}>I'm moving to the Vatican City!</a>
+                    <div className={styles.recentEntryDate}>31/03/2020</div>
                   </li>
                 </ul>
             </div>
 
             
-            <div class={styles.photoContainer}>
+            <div className={styles.photoContainer}>
 
-              <div class={styles.box} id="entry-1">
-                <div class={styles.imgBox}>
-                  <img src={lifeOfBrian} />
+              <div className={styles.box} id="entry-1">
+                <div className={styles.imgBox}>
+                  <img src={lifeOfBrian} alt="" />
                 </div>
-                <div class={styles.details}>
-                  <div class={styles.content}>
+                <div className={styles.details}>
+                  <div className={styles.content}>
                     <h3>Conjugate the verb!</h3>
                     <h5>31/03/20</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti obcaecati, quas voluptatibus adipisci corporis sit id provident animi quod dolorem hic nostrum mollitia voluptates reiciendis? Accusantium consectetur ut veniam aspernatur?</p>
@@ -58,12 +58,12 @@ export default function Blog() {
                 </div>
               </div>
 
-              <div class={styles.box} id="entry-2">
-                <div class={styles.imgBox}>
-                  <img src={lifeOfBrian} />
+              <div className={styles.box} id="entry-2">
+                <div className={styles.imgBox}>
+                  <img src={lifeOfBrian} alt="" />
                 </div>
-                <div class={styles.details}>
-                  <div class={styles.content}>
+                <div className={styles.details}>
+                  <div className={styles.content}>
                     <h3>Everyone loves latin!</h3>
                     <h5>31/03/20</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti obcaecati, quas voluptatibus adipisci corporis sit id provident animi quod dolorem hic nostrum mollitia voluptates reiciendis? Accusantium consectetur ut veniam aspernatur?</p>
@@ -71,12 +71,12 @@ export default function Blog() {
                 </div>
               </div>
 
-              <div class={styles.box} id="entry-3">
-                <div class={styles.imgBox}>
-                  <img src={lifeOfBrian} />
+              <div className={styles.box} id="entry-3">
+                <div className={styles.imgBox}>
+                  <img src={lifeOfBrian} alt="" />
                 </div>
-                <div class={styles.details}>
-                  <div class={styles.content}>
+                <div className={styles.details}>
+                  <div className={styles.content}>
                     <h3>What have the Romans ever done for us?</h3>
                     <h5>31/03/20</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti obcaecati, quas voluptatibus adipisci corporis sit id provident animi quod dolorem hic nostrum mollitia voluptates reiciendis? Accusantium consectetur ut veniam aspernatur?</p>
@@ -84,12 +84,12 @@ export default function Blog() {
                 </div>
               </div>
 
-              <div class={styles.box} id="entry-4">
-                <div class={styles.imgBox}>
-                  <img src={lifeOfBrian} />
+              <div className={styles.box} id="entry-4">
+                <div className={styles.imgBox}>
+                  <img src={lifeOfBrian} alt="" />
                 </div>
-                <div class={styles.details}>
-                  <div class={styles.content}>
+                <div className={styles.details}>
+                  <div className={styles.content}>
                     <h3>Learn a dead language??</h3>
                     <h5>31/03/20</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti obcaecati, quas voluptatibus adipisci corporis sit id provident animi quod dolorem hic nostrum mollitia voluptates reiciendis? Accusantium consectetur ut veniam aspernatur?</p>
@@ -97,11 +97,11 @@ export default function Blog() {
                 </div>
               </div>
 
-              <div class={styles.box} id="entry-5">
-                <div class={styles.imgBox}>
-                  <img src={lifeOfBrian} />
+              <div className={styles.box} id="entry-5">
+                <div className={styles.imgBox}>
+                  <img src={lifeOfBrian} alt="" />
                 </div>
-                <div class={styles.details}>
+                <div className={styles.details}>
                   <div class={styles.content}>
                     <h3>Moving to the Vatican City!</h3>
                     <h5>31/03/20</h5>
