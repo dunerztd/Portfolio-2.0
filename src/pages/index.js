8cb74e76c4dbc1384014f4884996c1e6ead14495
Tom D <dunerz@hotmail.com>
2021-04-22 13:41:46 +1000

    changed home page to be more responsive. at smaller screen sizes, smaller image and changing font sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from '../components/layout'
 
 import homeImage from '../files/home/background1.jpg'
+import homeImageSmall from '../files/home/background1_small.jpg'
 
 import "@fontsource/permanent-marker"
 import './index.css'
@@ -14,10 +15,12 @@ export default function App() {
     <Layout>
       <div className="main-view">
           <div className="background-image">
-            <img src={homeImage}  alt="" />
+            <img src={homeImage}  alt="" class="homeImage" />
+            <img src={homeImageSmall}  alt="" class="homeImageSmall" />
           </div>
           <div className="text">
-            <h1>Tom Dunstan | Web Developer</h1>
+            <h1>Tom Dunstan | </h1>
+            <h1>Web Developer</h1>
           </div>
       </div>
     </Layout>
